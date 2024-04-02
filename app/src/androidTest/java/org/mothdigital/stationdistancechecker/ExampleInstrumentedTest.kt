d8f385b30a186c14ff617443e82d873813b6ab09
Stanisław <stanislawmol97@gmail.com>
2024-04-02 12:33:49 +0200

    Improve CI pipeline with build and check tasks

--- app/src/androidTest/java/org/mothdigital/stationdistancechecker/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/org/mothdigital/stationdistancechecker/ExampleInstrumentedTest.kt
@@ -9,11 +9,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mothdigital.stationdistancechecker.design.theme.StationDistanceCheckerTheme
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
 
