b2f89dceb7e86f031270b43049715fa2217d92a1
Stanisław <stanislawmol97@gmail.com>
2024-04-02 16:40:39 +0200

    Fix StationSpanDomainModule

--- station-span/domain/src/main/java/org/mothdigital/station_span/domain/di/StationSpanDomainModule.kt
+++ station-span/domain/src/main/java/org/mothdigital/station_span/domain/di/StationSpanDomainModule.kt
@@ -1,7 +1,9 @@
 package org.mothdigital.station_span.domain.di
 
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
+import org.mothdigital.station_span.domain.usecase.CalculateDistanceUseCase
 
 val stationSpanDomainModule = module {
-
+    singleOf(::CalculateDistanceUseCase)
 }
