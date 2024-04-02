0404fcf545da213ada42512d363082b366cb801a
Stanisław <stanislawmol97@gmail.com>
2024-04-02 12:21:23 +0200

    UI tests

--- app/src/androidTest/java/org/mothdigital/stationdistancechecker/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/org/mothdigital/stationdistancechecker/ExampleInstrumentedTest.kt
@@ -1,12 +1,13 @@
 package org.mothdigital.stationdistancechecker
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
+import org.mothdigital.stationdistancechecker.design.theme.StationDistanceCheckerTheme
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +16,18 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("org.mothdigital.stationdistancechecker", appContext.packageName)
+    fun useAppContext() = runTest {
+        composeTestRule.setContent {
+            StationDistanceCheckerTheme {
+                Content()
+            }
+        }
+
+        delay(15_000)
     }
 }
